1c7e166b2ba0fed608c7ca5b81eb844a66c9ef1d
Jason Priem <jp@jasonpriem.org>
2012-05-26 18:03:51 -0400

    Displays GitHub latest commits. Yay, I'll be displayed

--- test/test_web.py
+++ test/test_web.py
@@ -1,5 +1,6 @@
 import unittest
-from nose.tools import nottest
+from nose.tools import nottest, assert_equals
+import json, os, requests
 
 from totalimpactwebapp import web
 
@@ -19,4 +20,49 @@ class TestWeb(unittest.TestCase):
     def test_about(self):
         res = self.app.get('/about')
         assert res.status == '200 OK', res.status
+
+class FakeResponse(object):
+    pass
+
+class FakeRequests():
+
+    my_resp = FakeResponse()
+
+    def get(self, url):
+
+        resp_type = self.get_resp_key(url)
+        resp_loc = os.path.join(
+            os.path.dirname(__file__),
+            "data",
+            "github_commits",
+            resp_type + ".json"
+            )
+            
+        self.my_resp.text = open(resp_loc).read()
+        return self.my_resp 
+
+    def get_resp_key(self, url):
+        if "webapp" in url:
+            return "webapp"
+        elif "core" in url:
+            return "core"
+        else:
+            raise Exception("unexpected url: " + url)
+
+class TestGithubCommits(unittest.TestCase):
+
+    def test_get(self):
+        commits = web.GithubCommits.get(FakeRequests())
+
+        assert_equals(len(commits), 59)
+
+    def test_get_returns_sorted_list(self):
+        commits = web.GithubCommits.get(FakeRequests())
+        assert isinstance(commits, list), type(commits)
+
+        assert_equals(
+            commits[0]["commit"]["author"]["date"],
+            "2012-05-25T14:06:10-07:00"
+            )
+
 
