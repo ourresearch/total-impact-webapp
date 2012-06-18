f7b7f983c7a03191824b796f024d980dc21f7beb
Jason Priem <jason@jasonpriem.com>
2012-06-17 20:48:27 -0700

    lots of design improvement

--- totalimpactwebapp/views.py
+++ totalimpactwebapp/views.py
@@ -12,11 +12,17 @@ logger = logging.getLogger("tiwebapp.views")
 # static pages
 @app.route('/')
 def home():
-    return render_template('index.html', body_class="homepage")
+    return render_template(
+    'index.html', 
+    page_title="uncover the invisible impact of research",
+    body_class="homepage")
 
 @app.route('/about')
 def about(): 
-    return render_template('about.html')
+    return render_template(
+        'about.html',
+        page_title="about"
+        )
 
 @app.route('/faq')
 def faq(): 
@@ -37,17 +43,25 @@ def faq():
     
     return render_template(
         'faq.html',
+        page_title="faq",
         which_artifacts=which_item_types,
         provider_metadata=metadata
         )
 
 @app.route('/api-docs')
 def apidocs(): 
-    return render_template('api-docs.html')
+    return render_template(
+        'api-docs.html',
+        page_title="api"
+        )
 
 @app.route('/collection/create')
 def collection_create():
-    return render_template('create-collection.html', body_class="create-collection")
+    return render_template(
+        'create-collection.html', 
+        page_title="create collection",
+        body_class="create-collection"
+        )
 
 @app.route('/collection/<collection_id>')
 def collection_report(collection_id):
@@ -56,17 +70,10 @@ def collection_report(collection_id):
     if r.status_code == 200:
         collection = json.loads(r.text)
         return render_template(
-        'collection.html',
-        collection=collection
+            'collection.html',
+            page_title=collection["title"],
+            collection=collection
         )
     else:
         abort(404, "This collection doesn't seem to exist yet.")
-        
-@app.route("/commits")
-def get_github_commits():
-    commits = Github.get_commits(requests)
-    resp = make_response( json.dumps(commits, sort_keys=True, indent=4), 200)        
-    resp.mimetype = "application/json"
-    return resp
-
 
