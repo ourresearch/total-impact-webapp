9843771e735f0deaee9e133b359cf3266172aea4
Jason Priem <jason@jasonpriem.com>
2012-07-26 18:53:47 -0400

    Calls /item instead of /items endpoint; assigns url namespace to urls.

--- totalimpactwebapp/static/js/total-impact-item.js
+++ totalimpactwebapp/static/js/total-impact-item.js
@@ -1,5 +1,5 @@
 function getLatestTimestamp(metricSnaps) {
-    var latestTs = "1999-01-01T00:00:00.000000"
+    var latestTs = "1999-01-01T00:00:00.000000";
     for (ts in metricSnaps) {
         if (ts > latestTs) {
             latestTs = ts
@@ -18,50 +18,42 @@ function sortByMetricValueDesc(metric1, metric2){
 }
 
 function renderItem(item){
-    item.metricsArr = []
+    item.metricsArr = [];
     for (metricName in item.metrics){
-        thisMetric = item.metrics[metricName]
-        thisMetric.name = metricName
-        var latestTs = getLatestTimestamp(item.metrics[metricName].values)
+        thisMetric = item.metrics[metricName];
+        thisMetric.name = metricName;
+        var latestTs = getLatestTimestamp(item.metrics[metricName].values);
         if (latestTs) {
-            var latestVal = item.metrics[metricName].values[latestTs]
-            thisMetric.ts = latestTs
+            var latestVal = item.metrics[metricName].values[latestTs];
+            thisMetric.ts = latestTs;
             thisMetric.value = latestVal
         }
 
         // if no values, no point in printing
         if (thisMetric.value) {
-            item.metricsArr.push(thisMetric)
+            item.metricsArr.push(thisMetric);
         }
     }
 
-    item.metricsArr.sort(sortByMetricValueDesc)
+    item.metricsArr.sort(sortByMetricValueDesc);
     
-    var url = (item.aliases.url) ?  item.aliases.url[0] : false
-    if (typeof ich.itemtemplate == "undefined"){ 
+    if (typeof ich.itemtemplate == "undefined"){
         ich.addTemplate("itemtemplate", '<div class=\"item\" id=\"{{ id }}\">\r\n  <div class=\"biblio\"><\/div>\r\n  <ul class=\"metrics\">{{#metricsArr}}\r\n    <li>\r\n      <a href=\"{{provenance_url}}\">\r\n        <span class=\"last-update\">{{ ts }}<\/span>\r\n        <span class=\"metric-value\">{{ value }}<\/span>\r\n        <span class=\"metric-name-img\">\r\n          <img src=\"{{ static_meta.icon }}\" width=\"16\" height=\"16\" border=\"0\">\r\n          <span class=\"metric-name\">{{ static_meta.provider }} {{ static_meta.display_name }}<\/span>\r\n        <\/span>\r\n      <\/a>\r\n    <\/li>\r\n    {{\/metricsArr}}\r\n  <\/ul>\r\n<\/div>')
     }
-    var html$ = ich.itemtemplate(item)
-    
-    return html$
+
+    return ich.itemtemplate(item);
 }
 
 function addDataToPage(data) {
-    $("#total-impact div#ti-data").empty()
-    for (i in data){
-        // make the set of all newly-rendered items
-        // this is a very slow way to do this...should bundle together,
-        // then make one replace.
-        $("#total-impact div#ti-data").append(renderItem(data[i]))
-    }
-}    
+    $("#total-impact div#ti-data").empty();
+    $("#total-impact div#ti-data").append(renderItem(data));
+}
 
  
-function getNewItemsAndUpdateReport(interval) {
-    tiidsStr = tiids.join(",")
+function getNewItemAndUpdateReport(tiid, interval) {
 
     $.ajax({
-        url: 'http://total-impact-core.herokuapp.com/items/'+tiidsStr,                        
+        url: 'http://api.total-impact.org/item/'+tiid,
         type: "GET",
         dataType: "json",
         contentType: "application/json; charset=utf-8",
@@ -75,7 +67,7 @@ function getNewItemsAndUpdateReport(interval) {
             },
             200: function(data) {
                 console.log("done with updating")
-                addDataToPage(data)                
+                addDataToPage(data)
                 return false;
             }
         }
@@ -83,14 +75,20 @@ function getNewItemsAndUpdateReport(interval) {
 }
 
 
-function getAliases(input) {
-    // get the user-supplied aliases
-   var aliases = [];
-   var thisAlias = [];
-   thisAlias[0] = input.split(':')[0]
-   thisAlias[1] = input.split(':')[1]
-   aliases.push(thisAlias)
-    return(aliases)
+function getAlias(id_string) {
+   // split the user-supplied alias string into namespace and id parts
+    var alias = []
+    if (id_string.indexOf(":") > 0) {
+        alias[0] = id_string.split(':')[0]; // namespace
+        alias[1] = id_string.substr(alias[0].length + 1); // id
+
+        // handle urls:
+        if (alias[0] == "http"){
+            alias[0] = "url";
+            alias[1] = id_string
+        }
+    }
+    return alias
 }
 
 
@@ -119,29 +117,27 @@ $.ajaxSetup ({
     cache: false
 });
 var tiids = []
-var ajax_load = "<img src='/static/img/ajax-loader.gif' alt='loading...' />";
+var ajax_load = '<img src="http://total-impact.org/static/img/ajax-loader.gif" alt="loading..." />';
 
 $(document).ready(function(){
-    // updating the collection from the report page
     //requestStylesheet("http://total-impact-webapp.herokuapp.com/static/embed.css")
     //requestScript("http://total-impact-webapp.herokuapp.com/static/js/icanhaz.min.js")
 
     $("span#ti-id").hide()
     $("#total-impact div#ti-data").html(ajax_load+" Loading...")
-    aliases = getAliases($("span#ti-id").html());
+    var alias = getAlias($("span#ti-id").html());
 
-    console.log("adding new items.")
+    console.log("creating new item")
     $.ajax({
-        url: "http://total-impact-core.herokuapp.com/items",                        
+        url: "http://api.total-impact.org/item/"+alias[0]+"/"+alias[1],
         type: "POST",
         dataType: "json",
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(aliases),
-        success: function(returnedTiids){
+        data: JSON.stringify(alias),
+        success: function(returnedTiid){
             // make a new collection, populated by our freshly-minted tiids
-            console.log("items created. ")
-            tiids = returnedTiids
-            getNewItemsAndUpdateReport(500)
+            console.log("item created. ")
+            getNewItemAndUpdateReport(returnedTiid, 500)
         }
     });
 });
