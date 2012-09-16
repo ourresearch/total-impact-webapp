feeeb61514b5c0f5d0093845e76ff7b47648a91d
Heather Piwowar <hpiwowar@gmail.com>
2012-09-15 20:01:36 -0700

    add scienceseeker to display

--- totalimpactwebapp/static/js/ti-item.js
+++ totalimpactwebapp/static/js/ti-item.js
@@ -37,6 +37,7 @@ function Item(dict, itemView) {
         "pubmed:pmc_citations": ["scholars", "cited", "glyph"],
         "pubmed:pmc_citations_editorials": ["scholars", "recommended", "zoom"],
         "pubmed:pmc_citations_reviews": ["scholars", "cited", "zoom"],
+        "scienceseeker:blog_posts": ["scholars", "discussed", "zoom"],
         "slideshare:comments": ["public", "discussed"],
         "slideshare:downloads": ["public", "viewed"],
         "slideshare:favorites": ["public", "saved"],
@@ -385,7 +386,10 @@ function ItemView() {
         item$.find("div.biblio").append(biblio$)
 
         var zoom$ = this.renderZoom(item.engagementTable, true)
-        item$.find("div.zoom").append(zoom$).hide()
+        item$.find("div.zoom").append(zoom$)
+
+        // In Heather's version don't hide
+        //item$.find("div.zoom").append(zoom$).hide()
 
         item$.hover(
             function(){
