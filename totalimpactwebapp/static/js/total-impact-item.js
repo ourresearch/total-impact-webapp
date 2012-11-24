d83e00326c13942235d870f5690f32eb590aad08
Jason Priem <jp@jasonpriem.org>
2012-11-24 00:49:21 -0500

    rips out much of old embed code, adds ability to use localhost for testing.

--- totalimpactwebapp/static/js/total-impact-item.js
+++ totalimpactwebapp/static/js/total-impact-item.js
@@ -1,4 +1,6 @@
 (function () {
+//    var webappRoot = "http://impactstory.org"
+    var webappRoot = "http://localhost:5000" // for testing
 
     var jQuery;
 
@@ -44,113 +46,13 @@
         if (!window.console) {
            console = {log: function() {}}; 
         }
-        
-        function getLatestTimestamp(metricSnaps) {
-            var latestTs = "1999-01-01T00:00:00.000000";
-            for (ts in metricSnaps) {
-                if (ts > latestTs) {
-                    latestTs = ts
-                }
-            }
-            return latestTs
-        }
 
-
-        function sortByMetricValueDesc(metric1, metric2){
-            if (typeof metric1.value != "number")
-                return 1
-            if (typeof metric2.value != "number")
-                return -1
-            if (metric1.value < metric2.value)
-                return 1;
-            if (metric1.value > metric2.value)
-               return -1;
-            return 0;
-        }
-
-        function renderItem(item) {
-            item.metricsArr = [];
-            for (metricName in item.metrics) {
-                thisMetric = item.metrics[metricName];
-                thisMetric.name = metricName;
-                var latestTs = getLatestTimestamp(item.metrics[metricName].values);
-                if (latestTs) {
-                    var latestVal = item.metrics[metricName].values[latestTs];
-                    thisMetric.ts = latestTs;
-                    thisMetric.value = latestVal
-                }
-
-                // if no values, no point in printing
-                if (thisMetric.value) {
-                    item.metricsArr.push(thisMetric);
-                }
-            }
-
-            // remove the dictionaries from what will be displayed
-            item.metricsArr = item.metricsArr.filter(function(x) {   
-                    good_to_print = (typeof x["value"] == "number")
-                    if (typeof x["value"] == "string") {
-                        good_to_print = true
-                    }
-                    return good_to_print
-                })
-
-            item.metricsArr.sort(sortByMetricValueDesc);
-
-            if (typeof ich.itemtemplate == "undefined") {
-                ich.addTemplate("itemtemplate", '<div class=\"item\" id=\"{{ id }}\">\r\n  <div class=\"biblio\"><\/div>\r\n  <ul class=\"metrics\">{{#metricsArr}}\r\n    <li>\r\n      <a href=\"{{provenance_url}}\">\r\n        <span class=\"last-update\">{{ ts }}<\/span>\r\n        <span class=\"metric-value\">{{ value }}<\/span>\r\n        <span class=\"metric-name-img\">\r\n          <img src=\"{{ static_meta.icon }}\" width=\"16\" height=\"16\" border=\"0\">\r\n          <span class=\"metric-name\">{{ static_meta.provider }} {{ static_meta.display_name }}<\/span>\r\n        <\/span>\r\n      <\/a>\r\n    <\/li>\r\n    {{\/metricsArr}}\r\n  <\/ul>\r\n<\/div>')
-            }
-
-            return ich.itemtemplate(item);
-        }
 
         function addDataToPage(data) {
             jQuery("div#ti-data")
                 .empty()
                 .append(renderItem(data));
         }
-
-
-        function getNewItemAndUpdateReport(namespace, nid, interval) {
-            jQuery.ajax({
-                       url:"http://api.impactstory.org/v1/item/" + namespace + "/" + nid + "?key=EXAMPLE",
-                       type:"GET",
-                       dataType:"json",
-                       contentType:"application/json; charset=utf-8",
-                       statusCode:{
-                           210:function (data) {
-                               console.log("still updating");
-                               addDataToPage(data);
-                               setTimeout(function () {
-                                   getNewItemAndUpdateReport(namespace, nid, interval)
-                               }, interval)
-                           },
-                           200:function (data) {
-                               console.log("done with updating");
-                               addDataToPage(data);
-                               return false;
-                           }
-                       }
-                   });
-        }
-
-
-        function getAlias(id_string) {
-            // split the user-supplied alias string into namespace and id parts
-            var alias = [];
-            if (id_string.indexOf(":") > 0) {
-                alias[0] = id_string.split(':')[0]; // namespace
-                alias[1] = id_string.substr(alias[0].length + 1); // id
-
-                // handle urls:
-                if (alias[0] == "http") {
-                    alias[0] = "url";
-                    alias[1] = id_string
-                }
-            }
-            return alias
-        }
-
 
         // Based on http://drnicwilliams.com/2006/11/21/diy-widgets/
         function requestStylesheet(stylesheet_url) {
@@ -172,14 +74,13 @@
             document.getElementsByTagName('head')[0].appendChild(script);
         }
 
-
         jQuery.ajaxSetup({ cache:false });
         var ajax_load = '<img id="ti-loading" src="http://impactstory.org/static/img/ajax-loader.gif" alt="loading..." />';
 
         jQuery(document).ready(function ($) {
-            requestStylesheet("http://total-impact.org/static/css/embed.css");
-//            requestStylesheet("http://localhost:5000/static/css/embed.css");
-            requestScript("http://total-impact.org/static/js/icanhaz.min.js");
+            requestStylesheet(webappRoot + "/static/css/embed.css");
+            requestScript(webappRoot + "/static/js/icanhaz.min.js");
+            requestScript(webappRoot + "/static/js/item.js");
 
             $("span#ti-id").hide();
             $("div#ti-data").html(ajax_load + " Loading...");
@@ -188,7 +89,7 @@
             var nid = alias[1];
 
             $.ajax({
-                url:"http://api.impactstory.org/v1/item/" + namespace + "/" + nid + "?key=EXAMPLE",
+                url: webappRoot + "/v1/item/" + namespace + "/" + nid + "?key=EXAMPLE",
                 type:"POST",
                 dataType:"json",
                 contentType:"application/json; charset=utf-8",
