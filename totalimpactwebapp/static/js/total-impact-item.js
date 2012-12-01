9f9158b2e89e5e926014aaeaf99fef4c22382735
Jason Priem <jp@jasonpriem.org>
2012-12-01 12:47:46 -0500

    v0 embed code still works.

--- totalimpactwebapp/static/js/total-impact-item.js
+++ totalimpactwebapp/static/js/total-impact-item.js
@@ -1,6 +1,9 @@
 (function () {
-//    var webappRoot = "http://impactstory.org"
-    var webappRoot = "http://localhost:5000" // for testing
+    var webappRoot = "http://impactstory.org"
+    var apiRoot = "http://api.impactstory.org"
+
+//    var webappRoot = "http://localhost:5000" // for testing
+//    var apiRoot = "http://localhost:5001" // for testing
 
     var jQuery;
 
@@ -39,62 +42,168 @@
     }
 
 
-    /******** utility functions ******/
-
-    // Based on http://drnicwilliams.com/2006/11/21/diy-widgets/
-    function requestStylesheet(stylesheet_url) {
-        var stylesheet = document.createElement("link");
-        stylesheet.rel = "stylesheet";
-        stylesheet.type = "text/css";
-        stylesheet.href = stylesheet_url;
-        stylesheet.media = "all";
-        document.lastChild.firstChild.appendChild(stylesheet);
-    }
-
-
-    // Based on http://drnicwilliams.com/2006/11/21/diy-widgets/
-    function requestScript(script_url) {
-        var script = document.createElement('script');
-        script.src = script_url;
-        // IE7 doesn't like this: document.body.appendChild(script);
-        // Instead use:
-        document.getElementsByTagName('head')[0].appendChild(script);
-    }
-
-
-    /******** ImpactStory functions ******/
-    function getItemId() {
-        var doi = jQuery("div#impactstory-embed").attr("data-doi")
-        console.log(doi)
-        return ["doi", doi]
-    }
-
-    function loadBadgesTemplate(webAppRoot) {
-        $.get(webAppRoot + "/static/")
-    }
-
-
     /******** Our main function ********/
 
     function main() {
-        jQuery.ajaxSetup({ cache:false });
-        var ajax_load = '<img id="ti-loading" src=""' + webappRoot + '/static/img/ajax-loader.gif" alt="loading..." />';
 
         if (!window.console) {
-           console = {log: function() {}}; 
+           console = {log: function() {}};
         }
 
-        requestStylesheet(webappRoot + "/static/css/embed.css");
-        requestScript(webappRoot + "/static/js/icanhaz.min.js");
-        requestScript(webappRoot + "/static/js/ti-item.js");
+        function getLatestTimestamp(metricSnaps) {
+            var latestTs = "1999-01-01T00:00:00.000000";
+            for (ts in metricSnaps) {
+                if (ts > latestTs) {
+                    latestTs = ts
+                }
+            }
+            return latestTs
+        }
 
+
+        function sortByMetricValueDesc(metric1, metric2){
+            if (typeof metric1.value != "number")
+                return 1
+            if (typeof metric2.value != "number")
+                return -1
+            if (metric1.value < metric2.value)
+                return 1;
+            if (metric1.value > metric2.value)
+               return -1;
+            return 0;
+        }
+
+        function renderItem(item) {
+            item.metricsArr = [];
+            for (metricName in item.metrics) {
+                thisMetric = item.metrics[metricName];
+                thisMetric.name = metricName;
+                var latestTs = getLatestTimestamp(item.metrics[metricName].values);
+                if (latestTs) {
+                    var latestVal = item.metrics[metricName].values[latestTs];
+                    thisMetric.ts = latestTs;
+                    thisMetric.value = latestVal
+                }
+
+                // if no values, no point in printing
+                if (thisMetric.value) {
+                    item.metricsArr.push(thisMetric);
+                }
+            }
+
+            // remove the dictionaries from what will be displayed
+            item.metricsArr = item.metricsArr.filter(function(x) {
+                    good_to_print = (typeof x["value"] == "number")
+                    if (typeof x["value"] == "string") {
+                        good_to_print = true
+                    }
+                    return good_to_print
+                })
+
+            item.metricsArr.sort(sortByMetricValueDesc);
+
+            if (typeof ich.itemtemplate == "undefined") {
+                ich.addTemplate("itemtemplate", '<div class=\"item\" id=\"{{ id }}\">\r\n  <div class=\"biblio\"><\/div>\r\n  <ul class=\"metrics\">{{#metricsArr}}\r\n    <li>\r\n      <a href=\"{{provenance_url}}\">\r\n        <span class=\"last-update\">{{ ts }}<\/span>\r\n        <span class=\"metric-value\">{{ value }}<\/span>\r\n        <span class=\"metric-name-img\">\r\n          <img src=\"{{ static_meta.icon }}\" width=\"16\" height=\"16\" border=\"0\">\r\n          <span class=\"metric-name\">{{ static_meta.provider }} {{ static_meta.display_name }}<\/span>\r\n        <\/span>\r\n      <\/a>\r\n    <\/li>\r\n    {{\/metricsArr}}\r\n  <\/ul>\r\n<\/div>')
+            }
+
+            return ich.itemtemplate(item);
+        }
+
+        function addDataToPage(data) {
+            jQuery("div#ti-data")
+                .empty()
+                .append(renderItem(data));
+        }
+
+
+        function getNewItemAndUpdateReport(namespace, nid, interval) {
+            jQuery.ajax({
+                       url: apiRoot+"/v1/item/" + namespace + "/" + nid + "?key=EXAMPLE",
+                       type:"GET",
+                       dataType:"json",
+                       contentType:"application/json; charset=utf-8",
+                       statusCode:{
+                           210:function (data) {
+                               console.log("still updating");
+                               addDataToPage(data);
+                               setTimeout(function () {
+                                   getNewItemAndUpdateReport(namespace, nid, interval)
+                               }, interval)
+                           },
+                           200:function (data) {
+                               console.log("done with updating");
+                               addDataToPage(data);
+                               return false;
+                           }
+                       }
+                   });
+        }
+
+
+        function getAlias(id_string) {
+            // split the user-supplied alias string into namespace and id parts
+            var alias = [];
+            if (id_string.indexOf(":") > 0) {
+                alias[0] = id_string.split(':')[0]; // namespace
+                alias[1] = id_string.substr(alias[0].length + 1); // id
+
+                // handle urls:
+                if (alias[0] == "http") {
+                    alias[0] = "url";
+                    alias[1] = id_string
+                }
+            }
+            return alias
+        }
+
+
+        // Based on http://drnicwilliams.com/2006/11/21/diy-widgets/
+        function requestStylesheet(stylesheet_url) {
+            var stylesheet = document.createElement("link");
+            stylesheet.rel = "stylesheet";
+            stylesheet.type = "text/css";
+            stylesheet.href = stylesheet_url;
+            stylesheet.media = "all";
+            document.lastChild.firstChild.appendChild(stylesheet);
+        }
+
+
+        // Based on http://drnicwilliams.com/2006/11/21/diy-widgets/
+        function requestScript(script_url) {
+            var script = document.createElement('script');
+            script.src = script_url;
+            // IE7 doesn't like this: document.body.appendChild(script);
+            // Instead use:
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
+
+
+        jQuery.ajaxSetup({ cache:false });
+        var ajax_load = '<img id="ti-loading" src="http://impactstory.org/static/img/ajax-loader.gif" alt="loading..." />';
+
+        requestStylesheet("http://total-impact.org/static/css/embed.css");
+//            requestStylesheet("http://localhost:5000/static/css/embed.css");
+        requestScript("http://total-impact.org/static/js/icanhaz.min.js");
         jQuery(document).ready(function ($) {
 
-            var $ = jQuery
-            var itemId = getItemId()
-            var item = new Item(itemId, new ItemView($), $)
+            $("span#ti-id").hide();
+            $("div#ti-data").html(ajax_load + " Loading...");
+            var alias = getAlias($("span#ti-id").html());
+            var namespace = alias[0];
+            var nid = alias[1];
 
-
+            $.ajax({
+                url:apiRoot +"/v1/item/" + namespace + "/" + nid + "?key=EXAMPLE",
+                type:"POST",
+                dataType:"json",
+                contentType:"application/json; charset=utf-8",
+                data:JSON.stringify(alias),
+                success:function (returnedTiid) {
+                   // make a new collection, populated by our freshly-minted tiids
+                   console.log("item created. ");
+                   getNewItemAndUpdateReport(namespace, nid, 1000);
+                }
+           });
         });
     }
 })()
