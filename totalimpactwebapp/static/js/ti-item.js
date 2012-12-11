1a5cffe3e01ecb19bcac52b1ef6834ed29316525
Jason Priem <jp@jasonpriem.org>
2012-12-11 13:12:10 -0500

    patch Array.filter() for IE8

--- totalimpactwebapp/static/js/ti-item.js
+++ totalimpactwebapp/static/js/ti-item.js
@@ -1,4 +1,35 @@
+// patch Array.filter() for IE8
+// from https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/filter#Compatibility
 
+if (!Array.prototype.filter)
+{
+    Array.prototype.filter = function(fun /*, thisp */)
+    {
+        "use strict";
+
+        if (this == null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+            if (i in t)
+            {
+                var val = t[i]; // in case fun mutates this
+                if (fun.call(thisp, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
 
 function Item(itemData, itemView, $) {
 
