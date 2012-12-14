2b4057fb785b39a8465d2b9b8746c003c4676516
Jason Priem <jp@jasonpriem.org>
2012-12-14 01:28:10 -0500

    adds vitals/ logging.

--- totalimpactwebapp/views.py
+++ totalimpactwebapp/views.py
@@ -191,7 +191,7 @@ def vitals():
     """
     vitals = request.json
 
-    logger.debug(json.dumps(vitals))
+    logger.debug(json.dumps("here are the vitals: " + vitals))
 #    embeds_per_page = len(vitals["allParams"])
 #    # heather does awesome things with the vitals and mixpanel here.
 #    logger.info("Got vitals message with embeds_per_page={embeds_per_page}".format(
