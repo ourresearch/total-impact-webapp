1f4482be706a81e3d8801bd30420ff3a13f3fcc4
Jason Priem <jp@jasonpriem.org>
2013-04-04 00:02:36 -0400

    Add SQLAlchemy and Flask-SQLAlchemy to requirements.txt

--- totalimpactwebapp/__init__.py
+++ totalimpactwebapp/__init__.py
@@ -1,22 +1,32 @@
 import os, logging, sys
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
+
+# set up logging
 # see http://wiki.pylonshq.com/display/pylonscookbook/Alternative+logging+configuration
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.DEBUG,
     format='[%(process)d] %(levelname)8s %(threadName)30s %(name)s - %(message)s'
 )
-
 logger = logging.getLogger("tiwebapp")
 
 
+# set up application
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+# set up configs
 
 # Setting ASSETS_DEBUG=True makes debugging easier by NOT minimizing the assets.
 # Production should have ASSETS_DEBUG=False
 # ASSETS_DEBUG=True is the default
 
 app.config["ASSETS_DEBUG"] = (os.getenv("ASSETS_DEBUG", "True") == "True")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
+
+# set up views
 from totalimpactwebapp import views
--- totalimpactwebapp/models.py
+++ totalimpactwebapp/models.py
@@ -1,4 +1,12 @@
+from totalimpactwebapp import db
 
 
-class User():
-    pass
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(64))
+    lastname = db.Column(db.String(64))
+    email = db.Column(db.String(120), unique=True)
+
+
+    def __repr__(self):
+        return '<User %r>' % (self.firstname + " " + self.lastname)
