957f0c7764f7c8464ac8e0639d8a2a031237b431
Heather Piwowar <hpiwowar@gmail.com>
2013-06-10 12:33:08 -0700

    updated mix panel library

--- totalimpactwebapp/static/js/mixpanel.js
+++ totalimpactwebapp/static/js/mixpanel.js
@@ -5,7 +5,6 @@
     var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(
     Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?g=a[f]=[]:
     f="mixpanel";g.people=g.people||[];h=['disable','track','track_pageview','track_links',
-    'track_forms','register','register_once','unregister','identify','alias','name_tag',
-    'set_config','people.set','people.increment','people.track_charge','people.append'];
+    'track_forms','register','register_once','unregister','identify','alias','name_tag','set_config',
+    'people.set','people.set_once','people.increment','people.track_charge','people.append'];
     for(e=0;e<h.length;e++)d(g,h[e]);a._i.push([b,c,f])};a.__SV=1.2;})(document,window.mixpanel||[]);
-    	
