1e586002cbef4846d7cfaec4b4240940f8d53616
Heather Piwowar <hpiwowar@gmail.com>
2013-07-03 07:29:39 -0700

    enable google universal analytics
    Just using it to track page views to start.

--- totalimpactwebapp/static/js/google-analytics.js
+++ totalimpactwebapp/static/js/google-analytics.js
@@ -1,5 +1,19 @@
+// account ids hardcoded for now, should pull out into configs
+
+
+// google universal analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-42217314-1', 'impactstory.org');
+ga('send', 'pageview');
+
+
+
+// clasic google analytics
 var _gaq = _gaq || [];
-// acct id hardcoded for now, should pull out into config
 _gaq.push(['_setAccount', 'UA-23384030-1']);
 _gaq.push(['_trackPageview']);
 
@@ -8,3 +22,4 @@ _gaq.push(['_trackPageview']);
  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
