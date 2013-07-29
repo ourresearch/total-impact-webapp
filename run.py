f1a2944ba523fb744d4b7fc4ceae9c4d8d5c5fc3
Jason Priem <jp@jasonpriem.org>
2013-07-28 18:23:12 -0700

    Adds New Relic test code to run.py; commented it out.

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+
+# set env variables for local running.
 import config
 config.set_env_vars_from_dot_env()
 
+"""
+this is just for troubleshooting New Relic...normally we dont' want
+it sending data from localhost.
+"""
+# import newrelic.agent
+# newrelic.agent.initialize('newrelic.ini')
+
+
 from totalimpactwebapp import app
 app.run(debug=True)
+
+
