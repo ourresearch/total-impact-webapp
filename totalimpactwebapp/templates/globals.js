5432054b1eefa75b914bf784310d55973a117ba4
Jason Priem <jp@jasonpriem.org>
2013-08-07 23:52:50 -0400

    Make console calls fail gracefully; fixes behavior in IE9

--- totalimpactwebapp/templates/globals.js
+++ totalimpactwebapp/templates/globals.js
@@ -1,3 +1,33 @@
+// console calls fail gracefully.
+// from https://github.com/liamnewmarch/console-shim/blob/master/console-shim.js
+if (!window.console) (function() {
+
+    var __console, Console;
+
+    Console = function() {
+        var check = setInterval(function() {
+            var f;
+            if (window.console && console.log && !console.__buffer) {
+                clearInterval(check);
+                f = (Function.prototype.bind) ? Function.prototype.bind.call(console.log, console) : console.log;
+                for (var i = 0; i < __console.__buffer.length; i++) f.apply(console, __console.__buffer[i]);
+            }
+        }, 1000);
+
+        function log() {
+            this.__buffer.push(arguments);
+        }
+
+        this.log = log;
+        this.error = log;
+        this.warn = log;
+        this.info = log;
+        this.__buffer = [];
+    };
+
+    __console = window.console = new Console();
+})();
+
 function setRoot(url){
     var vmLocalhost = "10.0.2.2"
 
