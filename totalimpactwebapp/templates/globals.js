dd249d4aeda40605151637f55be86ced9489f366
Jason Priem <jp@jasonpriem.org>
2013-08-07 22:29:54 -0400

    globals sets root urls, is rendered for every request.

--- totalimpactwebapp/templates/globals.js
+++ totalimpactwebapp/templates/globals.js
@@ -1,6 +1,11 @@
-var api_root='{{ g.roots.api }}'
-var api_root_pretty='{{ g.roots.api_pretty }}'
-var webapp_root='{{ g.roots.webapp }}'
-var webapp_root_pretty='{{ g.roots.webapp_pretty }}'
+function setRoot(url){
+    return url.replace("http:", window.location.protocol)
+}
+
+var api_root=setRoot('{{ g.roots.api }}')
+var api_root_pretty=setRoot('{{ g.roots.api_pretty }}')
+var webapp_root=setRoot('{{ g.roots.webapp }}')
+var webapp_root_pretty=setRoot('{{ g.roots.webapp_pretty }}')
+
 var api_key='{{ g.api_key }}'
-var request_url='{{ request_url }}'
+var request_url='{{ request_url }}' // i don't think we need, this, keeping for legacy code
--- totalimpactwebapp/views.py
+++ totalimpactwebapp/views.py
@@ -138,11 +138,12 @@ def load_globals():
         "webapp_pretty": os.getenv("WEBAPP_ROOT_PRETTY", os.getenv("WEBAPP_ROOT"))
     }
 
-
     g.segmentio_key = os.getenv("SEGMENTIO_KEY")
     g.mixpanel_token = os.getenv("MIXPANEL_TOKEN")
     g.api_key = os.getenv("API_KEY")
     g.newrelic_header = newrelic.agent.get_browser_timing_header()
+
+    g.js_globals = render_template("globals.js")
 
 
 @app.before_request
