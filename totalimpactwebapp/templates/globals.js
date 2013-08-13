5054c21fa60d5559859aeffa97f183f0c21a9dc1
Jason Priem <jp@jasonpriem.org>
2013-08-13 12:19:58 +0800

    debut code for widget https

--- totalimpactwebapp/templates/globals.js
+++ totalimpactwebapp/templates/globals.js
@@ -41,10 +41,13 @@ function setRoot(url){
     return url
 }
 
+console.log("loading globals")
 var api_root=setRoot('{{ g.roots.api }}')
 var api_root_pretty=setRoot('{{ g.roots.api_pretty }}')
 var webapp_root=setRoot('{{ g.roots.webapp }}')
 var webapp_root_pretty=setRoot('{{ g.roots.webapp_pretty }}')
+
+console.log("webapp root in ", webapp_root)
 
 var api_key='{{ g.api_key }}'
 var request_url='{{ request_url }}' // i don't think we need, this, keeping for legacy code
