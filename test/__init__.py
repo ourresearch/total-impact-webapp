3cd526a85b57858660b72ad9e649284c57beb3ef
Jason Priem <jp@jasonpriem.org>
2013-08-15 14:40:05 +0800

    Unit tests work.

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-import nose, os
-
-def setup():
-    pass
-
-
-def tearDown(self):
-    pass
--- test/test_user.py
+++ test/test_user.py
@@ -3,13 +3,14 @@ from nose.tools import nottest, assert_equals
 
 from totalimpactwebapp import user, app
 
-class TestViews(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
     def setUp(self):
         pass
 
     def test_file_loads(self):
         assert True
+
 
 
 
