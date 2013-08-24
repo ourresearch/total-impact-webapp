3ab6aa9f4b031cbe046b42b75c0bbf6d3ad3a6ad
Heather Piwowar <hpiwowar@gmail.com>
2013-08-24 09:56:12 -0700

    add more logging to figure out analytics flushing exception

--- totalimpactwebapp/views.py
+++ totalimpactwebapp/views.py
@@ -815,19 +815,26 @@ def widget_analytics():
     logger.info(u"got widget analytics data: {data}".format(
         data=d))
 
-    # later look stuff up here from db, based on api-key; send along w identify() call...
-    analytics.identify(user_id=api_key)
+    try:
+        # later look stuff up here from db, based on api-key; send along w identify() call...
+        analytics.identify(user_id=api_key)
+    except IndexError:
+        logger.debug(u"IndexError when doing analytics.identify in widget_analytics")
 
-    analytics.track(
-        user_id=api_key,
-        event="Served a page with embedded widget",
-        properties=d
-    )
+    try:
+        analytics.track(
+            user_id=api_key,
+            event="Served a page with embedded widget",
+            properties=d
+        )
+    except IndexError:
+        logger.debug(u"IndexError when doing analytics.track in widget_analytics")
+
     try:
         analytics.flush(async=False)  # make sure all the data gets sent to segment.io
     except IndexError:
         # sometimes the data was already flushed and we get an error popping from an empty queue
-        pass
+        logger.debug(u"IndexError when doing analytics.flush in widget_analytics")
 
     return make_response(request.args.get("callback", "") + '({"status": "success"})', 200)
 
