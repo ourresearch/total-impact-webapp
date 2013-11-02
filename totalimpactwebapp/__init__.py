3fc223702da5d4241fb6a4f6c9d344b21c702d57
Heather Piwowar <hpiwowar@gmail.com>
2013-11-02 07:08:48 -0700

    add database reconnection code and create database tables if they aren't there

--- totalimpactwebapp/__init__.py
+++ totalimpactwebapp/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
 
 # set up logging
 # see http://wiki.pylonshq.com/display/pylonscookbook/Alternative+logging+configuration
@@ -21,21 +24,44 @@ app = Flask(__name__)
 # http://flask.pocoo.org/mailinglist/archive/2011/2/27/re-automatic-removal-of-trailing-slashes/#043b1a0b6e841ab8e7d38bd7374cbb58
 app.url_map.strict_slashes = False
 
+# database stuff
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
+# from http://docs.sqlalchemy.org/en/latest/core/pooling.html
+# This recipe will ensure that a new Connection will succeed even if connections in the pool 
+# have gone stale, provided that the database server is actually running. 
+# The expense is that of an additional execution performed per checkout
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+
+
+# log in stuff
 login_manager = LoginManager()
 login_manager.setup_app(app)
 
 
-# set up configs
+# config and debugging stuff
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # Set env PACK_ASSETS=True to pack/minimize assets.
 # Set env PACK_ASSETS=False (default) to keep them in separate files.
 # Production should be PACK_ASSETS=True
 app.config["ASSETS_DEBUG"] = (os.getenv("PACK_ASSETS") != "True")
 
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # set up Flask-DebugToolbar
 if (os.getenv("FLASK_DEBUG", False) == "True"):
@@ -44,5 +70,10 @@ if (os.getenv("FLASK_DEBUG", False) == "True"):
     app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False    
 toolbar = DebugToolbarExtension(app)
 
-# set up views
-from totalimpactwebapp import views
+try:
+	from totalimpactwebapp import views
+except exc.ProgrammingError:
+	logger.info("SQLAlchemy database tables not found, so creating them")
+	db.session.rollback()
+	db.create_all()
+	from totalimpactwebapp import views
