9b5b1ff68f8eb24a3e7394e9e8df79f9fce60b3a
Jason Priem <jp@jasonpriem.org>
2013-11-26 09:05:22 -0800

    Make logged-in toolbar vertical dividers show even on landing page.

--- totalimpactwebapp/static/dist/ImpactStory.js
+++ totalimpactwebapp/static/dist/ImpactStory.js
@@ -1,4 +1,4 @@
-/*! ImpactStory - v0.0.1-SNAPSHOT - 2013-11-25
+/*! ImpactStory - v0.0.1-SNAPSHOT - 2013-11-26
  * http://impactstory.org
  * Copyright (c) 2013 ImpactStory;
  * Licensed MIT
@@ -5423,14 +5423,14 @@ angular.module("security/login/toolbar.tpl.html", []).run(["$templateCache", fun
     "   </li>\n" +
     "\n" +
     "   <li ng-show=\"currentUser\" class=\"preferences nav-item\">\n" +
-    "      <span ng-show=\"!page.isLandingPage()\" class=\"or\"></span>\n" +
+    "      <span class=\"or\"></span>\n" +
     "      <a class=\"profile preference\"\n" +
     "         href=\"/settings/profile\"\n" +
     "         tooltip=\"Change profile settings\"\n" +
     "         tooltip-placement=\"bottom\">\n" +
     "         <i class=\"icon-cog\"></i>\n" +
     "      </a>\n" +
-    "      <span ng-show=\"!page.isLandingPage()\" class=\"or\"></span>\n" +
+    "      <span class=\"or\"></span>\n" +
     "      <a class=\"logout preference\"\n" +
     "         ng-click=\"logout()\"\n" +
     "         tooltip=\"Log out\"\n" +
