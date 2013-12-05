0e28296df845988684d3f8e1c6dc889963e910fd
Heather Piwowar <hpiwowar@gmail.com>
2013-12-04 20:22:41 -0800

    enable github repostory urls tab

--- totalimpactwebapp/static/dist/ImpactStory.js
+++ totalimpactwebapp/static/dist/ImpactStory.js
@@ -136,28 +136,30 @@ angular.module('importers.allTheImporters')
       displayName: "GitHub",
       url: 'http://github.com',
       descr: "GitHub is an online code repository emphasizing community collaboration features.",
-//      tabs: [
-//        {
-//          label: "account"
-//        },
-//        {
-//          label: "individual repositories"
-//        }
-//      ],
+     tabs: [
+       {
+         label: "account"
+       },
+       {
+         label: "individual repositories"
+       }
+     ],
       inputs: [{
-        tab: 0,
-        inputType: "username",
-        inputNeeded: "username",
-        help: "Your GitHub account ID is at the top right of your screen when you're logged in.",
-        saveUsername: true
-      }
-//      ,{
-//        tab:1,
-//        inputType: "idList",
-//        inputNeeded: "URLs",
-//        help: "Past URLs for other github repos here.",
-//        placeholder: "https://github.com/cboettig/knitcitations"
-//      }
+            tab: 0,
+            name: "account_name",            
+            inputType: "username",
+            inputNeeded: "username",
+            help: "Your GitHub account ID is at the top right of your screen when you're logged in.",
+            saveUsername: true
+          }
+         ,{
+            tab:1,
+            name: "repository_urls",                        
+            inputType: "idList",
+            inputNeeded: "URLs",
+            help: "Paste URLs for other github repositories here.",
+            placeholder: "https://github.com/cboettig/knitcitations"
+         }
       ]
     },
 
@@ -2054,6 +2056,7 @@ angular.module( 'signup', [
 
 ;
 
+angular.module("update.update",["resources.users"]).factory("Update",function(e,t,n,r,i){var s={},o=function(e,t){s.numNotDone>0||_.isNull(s.numNotDone)?n.query({id:e,idType:"url_slug"},function(n){s.numDone=u(n,!0);s.numNotDone=u(n,!1);s.percentComplete=s.numDone*100/(s.numDone+s.numNotDone);console.log("in keepPolling");console.log(s);r(function(){o(e,t)},500)}):t()},u=function(e,t){var n=_.filter(e,function(e){return!e.currently_updating});return t?n.length:e.length-n.length},a=function(e,t){s.numDone=null;s.numNotDone=null;s.percentComplete=null;var n=i.open({templateUrl:"update/update-progress.tpl.html",controller:"updateProgressModalCtrl",backdrop:"static",keyboard:!1});o(e,function(){n.close();t()})};return{showUpdate:a,updateStatus:s}}).controller("updateProgressModalCtrl",function(e,t){e.updateStatus=t.updateStatus});
 angular.module( 'update.update', [
     'resources.users'
   ])
@@ -2729,6 +2732,7 @@ angular.module('resources.products',['ngResource'])
 })
 
 
+angular.module("resources.users",["ngResource"]).factory("Users",function(e){return e("/user/:id?id_type=:idType",{idType:"userid"})}).factory("UsersProducts",function(e){return e("/user/:id/products?id_type=:idType&include_heading_products=:includeHeadingProducts",{idType:"url_slug",includeHeadingProducts:!1},{update:{method:"PUT"},patch:{method:"POST",headers:{"X-HTTP-METHOD-OVERRIDE":"PATCH"}},"delete":{method:"DELETE",headers:{"Content-Type":"application/json"}},query:{method:"GET",isArray:!0,cache:!0},poll:{method:"GET",isArray:!0,cache:!1}})}).factory("UsersProduct",function(e){return e("/user/:id/product/:tiid?id_type=:idType",{idType:"url_slug"},{update:{method:"PUT"}})}).factory("UsersAbout",function(e){return e("/user/:id/about?id_type=:idType",{idType:"url_slug"},{patch:{method:"POST",headers:{"X-HTTP-METHOD-OVERRIDE":"PATCH"},params:{id:"@about.id"}}})}).factory("UsersPassword",function(e){return e("/user/:id/password?id_type=:idType",{idType:"url_slug"})}).factory("UsersProductsCache",function(e){var t=[];return{query:function(){}}});
 angular.module('resources.users',['ngResource'])
 
   .factory('Users', function ($resource) {
