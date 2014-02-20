9cb400f852410ec737a670e4de9337ff6445457f
Jason Priem <jp@jasonpriem.org>
2014-02-20 10:40:02 -0800

    remove tiAnalytics.js

--- totalimpactwebapp/static/dist/ImpactStory.js
+++ totalimpactwebapp/static/dist/ImpactStory.js
@@ -1,4 +1,4 @@
-/*! ImpactStory - v0.0.1-SNAPSHOT - 2014-02-19
+/*! ImpactStory - v0.0.1-SNAPSHOT - 2014-02-20
  * http://impactstory.org
  * Copyright (c) 2014 ImpactStory;
  * Licensed MIT
@@ -3771,84 +3771,6 @@ angular.module('services.slug')
 
 
 })
-var analytics = analytics || {};
-
-angular.module('services.tiAnalytics', [
-//    'services.page'
-  ])
-  .run(['$http', function($http) {
-
-    // this is where you'd initialize GA, but segment.io is doing this for us.
-
-}])
-  .factory('tiAnalytics', function($window, $location, $routeParams) {
-
-//	$rootScope.$on('$viewContentLoaded', track);
-
-
-
-    var getPageType = function(){
-      var myPageType = "profile"
-
-
-      var pageTypeLookupTable = {
-        account: [
-          "/settings",
-          "/reset-password"
-        ],
-        landing: [
-          "/"
-        ],
-        infopage: [
-          "/faq",
-          "/about"
-        ],
-        signup: [
-          "/signup"
-        ]
-      }
-
-      _.each(pageTypeLookupTable, function(urlStartsWithList, pageType){
-        var filtered = _.filter(urlStartsWithList, function(x){
-           return _($location.path()).startsWith(x)
-        })
-        if (filtered.length) {
-          myPageType = pageType
-        }
-
-      })
-      return myPageType
-    }
-
-
-    var trackPageLoad = function(){
-      analytics.page(getPageType(), $location.path(), {
-
-      })
-    }
-
-
-
-	
-	var convertPathToQueryString = function(path, $routeParams) {
-		for (var key in $routeParams) {
-			var queryParam = '/' + $routeParams[key];
-			path = path.replace(queryParam, '');
-		}
-
-		var querystring = decodeURIComponent($.param($routeParams));
-
-		if (querystring === '') return path;
-
-		return path + "?" + querystring;
-	};
-
-
-  return {
-    'pageload': trackPageLoad
-
-  }
-});
 angular.module("services.timer", [])
 .factory("Timer", function(){
     var jobs = []
