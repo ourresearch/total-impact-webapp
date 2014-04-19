900c32a8f82645d1dffe061119ea1279b67d396b
Jason Priem <jp@jasonpriem.org>
2014-04-18 21:39:17 -0700

    don't show delete product unless current user owns profile.

--- totalimpactwebapp/static/dist/ImpactStory.js
+++ totalimpactwebapp/static/dist/ImpactStory.js
@@ -5317,7 +5317,7 @@ angular.module("profile/profile.tpl.html", []).run(["$templateCache", function($
     "             id=\"{{ product._id }}\"\n" +
     "             on-repeat-finished>\n" +
     "\n" +
-    "            <div class=\"single-product-controls\">\n" +
+    "            <div class=\"single-product-controls\" ng-show=\"currentUserIsProfileOwner()\">\n" +
     "               <a class=\"remove-product\"\n" +
     "                  tooltip=\"Delete this product\"\n" +
     "                  ng-click=\"removeProduct(product)\">\n" +
