c37ce96cbb697c13501ed1b18d53da637e3815d8
Heather Piwowar <hpiwowar@gmail.com>
2014-05-06 21:46:30 -0700

    add temp create_cards

--- daily.py
+++ daily.py
@@ -1,8 +1,11 @@
 from totalimpactwebapp.user import User
 from totalimpactwebapp import db
-import datetime
-
 import tasks
+
+import datetime
+from sqlalchemy.exc import IntegrityError, DataError, InvalidRequestError
+from sqlalchemy.orm.exc import FlushError
+
 
 
 
@@ -10,6 +13,7 @@ import tasks
 requires these env vars be set in this environment:
 DATABASE_URL
 """
+
 
 
 
@@ -37,30 +41,37 @@ def deduplicate_everyone():
 
 
 
-def put_linked_account_users_on_queue():
 
-    i = 0
-    # now = datetime.datetime.utcnow().isoformat()
-    now = "2013-06-24"
-    # for user in page_query(User.query.filter(User.next_refresh < now).order_by(User.next_refresh.asc())):
-    # for user in page_query(User.query.filter(User.next_refresh <= now)):
-    # for user in page_query(User.query):
-        # linked_accounts_to_sync = {
-        #     "figshare": user.figshare_id, 
-        #     "github": user.github_id, 
-        #     "orcid": user.orcid_id, 
-        #     "slideshare": user.slideshare_id 
-        # }
-        # has_linked_account = [account for account in linked_accounts_to_sync if linked_accounts_to_sync[account]]
-        # if has_linked_account:
-        #     i += 1
-        #     print u"{i} user {url_slug} has linked account: {has_linked_account} {next_refresh} ".format(
-        #         i=i, url_slug=user.url_slug, has_linked_account=has_linked_account, next_refresh=user.next_refresh)
-        #     for account in has_linked_account:
-        #         tiids = update_from_linked_account.delay(user, account)    
+
+
+class ProductNewMetricFactGenerator:
+    def __init__(self, User):
+        self.user = User
+
+    def make(self):
+        print "Hi I'm making some cards for", self.user.url_slug
+        # save all cards
+        return ["a"]
+
+
+def create_cards():
+    for user in page_query(User.query.order_by(User.url_slug.asc())):
+        facts = []
+        facts += ProductNewMetricFactGenerator(user)
+
+
+        for fact in facts:
+            db.session.add(fact)
+        
+        try:
+            db.session.commit()
+        except InvalidRequestError:
+            db.session.rollback()
+            db.session.commit()
+
 
 
 db.create_all()
-add_profile_deets_for_everyone()
+create_cards()
 
 
