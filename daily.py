a3238d92b2e4090b27cc4bd6230922ce4a967704
Heather Piwowar <hpiwowar@gmail.com>
2014-05-08 13:11:17 -0700

    can call with a single url slug

--- daily.py
+++ daily.py
@@ -2,6 +2,10 @@ from totalimpactwebapp.user import User
 from totalimpactwebapp import db
 import tasks
 
+import argparse
+from sqlalchemy import func
+
+
 
 
 
@@ -17,10 +21,10 @@ def page_query(q):
     offset = 0
     while True:
         r = False
-        for elem in q.limit(100).offset(offset):
+        for elem in q.limit(5).offset(offset):
            r = True
            yield elem
-        offset += 100
+        offset += 5
         if not r:
             break
 
@@ -37,27 +41,38 @@ def deduplicate_everyone():
 
 
 
-def create_cards_for_everyone():
-    for user in page_query(User.query.order_by(User.url_slug.asc())):
+
+def create_cards_for_everyone(url_slug=None):
+    if url_slug:
+        user = User.query.filter(func.lower(User.url_slug) == func.lower(url_slug)).first()
         print user.url_slug        
-        tasks.create_cards.delay(user)
+        # tasks.create_cards.delay(user)
+        tasks.create_cards(user)
+    else:    
+        for user in page_query(User.query.order_by(User.url_slug.asc())):
+            print user.url_slug        
+            tasks.create_cards.delay(user)
+
+
+def main(function, url_slug):
+    if function=="create_cards_for_everyone":
+        create_cards_for_everyone(url_slug)
 
 
 
 
-db.create_all()
-create_cards_for_everyone()
+if __name__ == "__main__":
 
-
-# if __name__ == "__main__":
-#     # get args from the command line:
-#     parser = argparse.ArgumentParser(description="Run periodic metrics updating from the command line")
-#     parser.add_argument('--url_slug', default=None, type=str, help="url slug")
-#     parser.add_argument('--number_to_update', default='3', type=int, help="Number to update.")
-#     parser.add_argument('--max_days_since_updated', default='7', type=int, help="Update if hasn't been updated in this many days.")
-#     args = vars(parser.parse_args())
-#     print args
-#     print u"updater.py starting."
-#     main(args["number_to_update"], args["max_days_since_updated"], [args["url_slug"]])
+    db.create_all()
+    
+    # get args from the command line:
+    parser = argparse.ArgumentParser(description="Run stuff")
+    parser.add_argument('--url_slug', default=None, type=str, help="url slug")
+    # parser.add_argument('--celery', default=True, type=bool, help="celery")
+    parser.add_argument('--function', default="create_cards_for_everyone", type=str, help="function")
+    args = vars(parser.parse_args())
+    print args
+    print u"daily.py starting."
+    main(args["function"], args["url_slug"])
 
 
