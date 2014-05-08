aba0de4a3503e4b90149d88c906675499443aa34
Heather Piwowar <hpiwowar@gmail.com>
2014-05-08 09:27:48 -0700

    add a print

--- daily.py
+++ daily.py
@@ -39,6 +39,7 @@ def deduplicate_everyone():
 
 def create_cards_for_everyone():
     for user in page_query(User.query.order_by(User.url_slug.asc())):
+        print user.url_slug        
         tasks.create_cards.delay(user)
 
 
@@ -46,5 +47,17 @@ def create_cards_for_everyone():
 
 db.create_all()
 create_cards_for_everyone()
+
+
+# if __name__ == "__main__":
+#     # get args from the command line:
+#     parser = argparse.ArgumentParser(description="Run periodic metrics updating from the command line")
+#     parser.add_argument('--url_slug', default=None, type=str, help="url slug")
+#     parser.add_argument('--number_to_update', default='3', type=int, help="Number to update.")
+#     parser.add_argument('--max_days_since_updated', default='7', type=int, help="Update if hasn't been updated in this many days.")
+#     args = vars(parser.parse_args())
+#     print args
+#     print u"updater.py starting."
+#     main(args["number_to_update"], args["max_days_since_updated"], [args["url_slug"]])
 
 
