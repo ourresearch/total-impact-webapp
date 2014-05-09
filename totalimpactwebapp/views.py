81cae7e210f92b1bb2bc4bb46ad20b6f1fd2d44f
Jason Priem <jp@jasonpriem.org>
2014-05-09 01:08:41 -0700

    build out report.html

--- totalimpactwebapp/views.py
+++ totalimpactwebapp/views.py
@@ -795,7 +795,7 @@ def render_cards(profile_id, granularity="all"):
 
 
     card_dicts = [card.to_dict() for card in cards]
-    return render_template("scratchpad.html")
+    return json_resp_from_thing(card_dicts)
 
 
 @app.route("/<profile_id>/report")
@@ -805,9 +805,9 @@ def render_report(profile_id):
         request
     )
     cards = Card.query.filter(Card.user_id == user.id).all()
+    print cards
 
-    return "\n".join([card.to_html() for card in cards])
-    # return render_template("report.html")
+    return render_template("report.html", user={}, cards=cards)
 
 
 @app.route("/test/email")
