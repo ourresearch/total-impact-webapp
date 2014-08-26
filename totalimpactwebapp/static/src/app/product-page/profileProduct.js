8b5103f46a2b460476af4bb0a3488764e7aef4a7
Jason Priem <jp@jasonpriem.org>
2014-08-26 12:58:14 -0700

    get https version of google pdf viewer.

--- totalimpactwebapp/static/src/app/product-page/profileProduct.js
+++ totalimpactwebapp/static/src/app/product-page/profileProduct.js
@@ -89,17 +89,17 @@ angular.module("productPage", [
     // once we've got a product.file_url set by the server,
     // delete them.
     // product.file_url = "http://www.slideshare.net/hpiwowar/right-time-right-place-to-change-the-world"
-    // product.file_url = "http://jasonpriem.org/self-archived/data-for-free.pdf"
+    product.file_url = "http://jasonpriem.org/self-archived/data-for-free.pdf"
     // product.file_url = "http://www.slideshare.net/hpiwowar/7-data-citation-challenges-illustrated-with-data-includes-elephants"
     // product.file_url = "https://gitprint.com/hpiwowar/Kira/blob/master/README.md?download"
 
     if (product.file_url){
       Embedly.get(
-        {url: product.file_url, secure: true},
+        {url: product.file_url},
         function(resp){
           console.log("successful resp from embedly: ", resp)
           if (resp.html) {
-            $scope.iframeToEmbed = resp.html          
+            $scope.iframeToEmbed = resp.html.replace("http://docs.google", "https://docs.google")
           } else {
             $scope.iframeToEmbed = '<iframe>' + resp.thumbnail + '</iframe>'   
             // http://api.embed.ly/1/oembed?url=https%3A%2F%2Fgithub.com%2Fhpiwowar%2FKira&maxwidth=500                 
