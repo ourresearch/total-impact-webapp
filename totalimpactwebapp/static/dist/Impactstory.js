f021ff2cd8c465678660e330ae9c6ac1c3a4283c
Jason Priem <jp@jasonpriem.org>
2014-10-18 17:56:02 -0700

    reword last-chance email.

--- totalimpactwebapp/static/dist/Impactstory.js
+++ totalimpactwebapp/static/dist/Impactstory.js
@@ -967,36 +967,6 @@ angular.module("genrePage", [
   }
 })
 
-.factory("SelectedProducts", function(){
-  var tiids = []
-
-  return {
-    add: function(tiid){
-      return tiids.push(tiid)
-    },
-    addFromObjects: function(objects){
-      return tiids = _.pluck(objects, "tiid")
-    },
-    remove: function(tiid){
-      tiids = _.without(tiids, tiid)
-    },
-    removeAll: function(){
-      return tiids.length = 0
-    },
-    contains: function(tiid){
-      return _.contains(tiids, tiid)
-    },
-    containsAny: function(){
-      return tiids.length > 0
-    },
-    get: function(){
-      return tiids
-    },
-    count: function(){
-      return tiids.length
-    }
-  }
-})
 
 
 
