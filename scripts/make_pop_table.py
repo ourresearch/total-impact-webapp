fdc550c2c9fe7c6007bf2fc48b6267c42ba3e8ef
Jason Priem <jp@jasonpriem.org>
2014-10-27 16:59:17 -0700

    script to make dict of country populations.

--- scripts/make_pop_table.py
+++ scripts/make_pop_table.py
@@ -2,6 +2,16 @@
 Joins a list of country populations and a list of alpha-2 iso country codes.
 
 Run from ~/projects/total-impact-webapp/scripts
+
+country_populations.csv comes from many sources, compiled by the World Bank:
+http://databank.worldbank.org/data/views/reports/tableview.aspx. It's been
+slightly modified when some of the codes were wrong.
+
+iso_country_codes comes from http://datahub.io/dataset/iso-3166-1-alpha-2-country-codes/resource/9c3b30dd-f5f3-4bbe-a3cb-d7b2c21d66ce
+and had to have around 10 lines modified, updated, or added using wikipedia data
+* http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+* http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+
 """
 
 import csv
