ef33c2772a39334e2656bfccffad63fd484c93e9
Jason Priem <jp@jasonpriem.org>
2014-10-28 14:05:21 -0700

    fix too long snap window from testing.

--- totalimpactwebapp/product.py
+++ totalimpactwebapp/product.py
@@ -40,7 +40,7 @@ percentile_snap_creations = 0
 logger = logging.getLogger("tiwebapp.product")
 deprecated_genres = ["twitter", "blog"]
 
-ignore_snaps_older_than = arrow.utcnow().replace(days=-122).datetime
+ignore_snaps_older_than = arrow.utcnow().replace(days=-22).datetime
 
 snaps_join_string = "and_(Product.tiid==Snap.tiid, " \
                     "Snap.last_collected_date > '{ignore_snaps_older_than}')".format(
