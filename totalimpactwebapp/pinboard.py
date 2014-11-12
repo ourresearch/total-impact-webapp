042f690d4b99474aa49e9dcaeaa922680020abb7
Jason Priem <jp@jasonpriem.org>
2014-11-11 16:33:51 -0800

    you can log in with your URL slug

--- totalimpactwebapp/pinboard.py
+++ totalimpactwebapp/pinboard.py
@@ -14,14 +14,24 @@ def set_key_products(profile_id, key_products_ids):
     return write_to_pinboard(profile_id, key_products_ids, "one")
 
 
-def new_contents_dict(col_pins_list, col_name):
+def new_contents_dict():
     contents_dict = {
         "one": [],
         "two": []
     }
-    contents_dict[col_name] = col_pins_list
     return contents_dict
 
+
+def save_new_board(profile_id):
+    contents = new_contents_dict()
+    board = Pinboard(
+        profile_id=profile_id,
+        contents=contents)
+
+    logger.info(u"saving board for {profile_id}: new contents: {contents}".format(
+        profile_id=profile_id, contents=contents))
+
+    return board
 
 def write_to_pinboard(profile_id, list_of_pins, col):
     board = Pinboard.query.filter_by(profile_id=profile_id).first()
@@ -31,18 +41,17 @@ def write_to_pinboard(profile_id, list_of_pins, col):
         try:
             board.contents[col] = list_of_pins
         except TypeError:
-            contents = new_contents_dict(list_of_pins, col)
-            board.contents = contents
+            new_contents = new_contents_dict()
+            new_contents[col] = list_of_pins
+            board.contents = new_contents
 
         board.timestamp = datetime.datetime.utcnow()
     else:
-        contents = new_contents_dict(list_of_pins, col)
-        board = Pinboard(
-            profile_id=profile_id,
-            contents=contents)
+        board = save_new_board(profile_id)
+        board.contents[col] = list_of_pins
 
         logger.info(u"saving board for {profile_id}: new contents: {contents}".format(
-            profile_id=profile_id, contents=contents))
+            profile_id=profile_id, contents=board.contents))
     db.session.merge(board)
     commit(db)
     return list_of_pins
