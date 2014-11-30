acd21fa0cbbae4b617e458b4658f85868ea519a4
Heather Piwowar <hpiwowar@gmail.com>
2014-11-30 07:27:06 -0800

    don't try to scrape biblio from pubmed pages

--- totalimpact/providers/webpage.py
+++ totalimpact/providers/webpage.py
@@ -51,7 +51,13 @@ class Webpage(Provider):
             biblio = aliases_dict["biblio"][0]
         elif "url" in aliases_dict:
             url = aliases_dict["url"][0]
-            if url and ("http://www.scopus.com/inward" not in url):
+            url_fragments_to_exclude = [
+                "scopus.com/inward",
+                "ncbi.nlm.nih.gov/pubmed"
+                "doi.org/"
+            ]
+
+            if url and any(fragment in url for fragment in url_fragments_to_exclude):
                 if not provider_url_template:
                     provider_url_template = self.biblio_url_template
                 biblio = self.get_biblio_for_id(url, provider_url_template, cache_enabled)
