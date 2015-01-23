566eda8b5312a147b7b48550f6a71ef1e0ac2d1b
Heather Piwowar <hpiwowar@gmail.com>
2015-01-23 14:38:58 -0800

    error check

--- totalimpactwebapp/tweet.py
+++ totalimpactwebapp/tweet.py
@@ -55,8 +55,9 @@ def store_tweet_payload_and_tweeter_from_twitter(payload_dicts_from_twitter, twe
                         tweeter.set_attributes_from_twitter_data(payload_dict["user"])
                         tweet.tweeter = tweeter
                         commit(db)
-                    logger.info(u"updated tweeter followers for {screen_name}".format(
-                        screen_name=tweet.tweeter.screen_name))
+                    if tweet.tweeter:
+                        logger.info(u"updated tweeter followers for {screen_name}".format(
+                            screen_name=tweet.tweeter.screen_name))
             
 
 def flag_deleted_tweets(tweet_ids):
